b5ad7c27a0bf5370b0b509380059f06026a92c5e
dorigodev <murillodorigo@gmail.com>
2025-05-23 14:19:56 -0300

    feat: create endpoint and business logic to search, update and delete category

--- src/main/java/com/spring/ecommerce/api/product/controller/CategoryController.java
+++ src/main/java/com/spring/ecommerce/api/product/controller/CategoryController.java
@@ -2,12 +2,14 @@ package com.spring.ecommerce.api.product.controller;
 
 import com.spring.ecommerce.api.product.dto.CategoryRequestDTO;
 import com.spring.ecommerce.api.product.dto.CategoryResponseDTO;
-import com.spring.ecommerce.api.product.mapper.CategoryMapper;
 import com.spring.ecommerce.api.product.service.CategoryService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("api/category")
 @AllArgsConstructor
+@Validated
 public class CategoryController {
 
     private final CategoryService service;
-    private final CategoryMapper mapper;
 
     @PostMapping
     public ResponseEntity<CategoryResponseDTO> createCategory(@RequestBody @Valid CategoryRequestDTO request) {
@@ -29,5 +31,21 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<List<CategoryResponseDTO>> getAllCategories() {
         return ResponseEntity.ok(service.getAllCategories());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponseDTO> getCategory(@PathVariable @NotNull @Positive Long id) {
+        return ResponseEntity.ok(service.getCategoryById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponseDTO> updateCategoryById(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid CategoryRequestDTO request) {
+        return ResponseEntity.ok(service.updateCategoryById(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable @NotNull @Positive Long id) {
+        service.deleteCategoryById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/spring/ecommerce/api/product/service/CategoryService.java
+++ src/main/java/com/spring/ecommerce/api/product/service/CategoryService.java
@@ -5,7 +5,9 @@ import com.spring.ecommerce.api.product.dto.CategoryResponseDTO;
 import com.spring.ecommerce.api.product.mapper.CategoryMapper;
 import com.spring.ecommerce.api.product.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,6 +29,22 @@ public class CategoryService {
                 .stream()
                 .map(mapper::toCategoryResponseDTO)
                 .toList();
+    }
+
+    public CategoryResponseDTO getCategoryById(Long id) {
+        return mapper.toCategoryResponseDTO(repository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NO_CONTENT)));
+    }
+
+    public CategoryResponseDTO updateCategoryById(Long id, CategoryRequestDTO request) {
+      var existingCategory = repository.findById(id)
+              .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+      mapper.updateCategoryFromDto(request,repository.findById(id).orElseThrow());
+      return mapper.toCategoryResponseDTO(repository.save(existingCategory));
+    }
+
+    public void deleteCategoryById(Long id) {
+        repository.deleteById(id);
     }
 
 }
